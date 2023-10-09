63b202ba97cb89eb60dcadf0f45eb471bf523668
elham34 <elham.alwadie@gmail.com>
2023-10-09 10:54:28 +0300

    Elham commit

--- Elham.swift
+++ Elham.swift
@@ -5,4 +5,35 @@
 //  Created by Elham on 24/03/1445 AH.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct ElhamView: View {
+    var body: some View {
+        
+        VStack (alignment: .leading) {
+            
+            Image("img2")
+            
+                .clipShape(Circle())
+                .overlay{
+                                Circle().stroke(.red, lineWidth: 4)
+                            }
+                .shadow(radius: 10)
+                       
+            Text(" Elham Ahmed")
+                .font(.title)
+                .foregroundColor(.red)
+            Text("I major in computer scince and prever programing and i love learn alot")
+                           .font(.subheadline)
+                           .foregroundStyle(.black)
+            
+        }
+        
+    }
+}
+
+
+#Preview {
+    ElhamView()
+}
