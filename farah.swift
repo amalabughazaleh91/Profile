8a0415725d8ed094dbd8b7e6319800244019ffb0
farah alshammari <farah.ntuf@outlook.sa>
2023-10-09 10:40:23 +0300

    farah commit

--- farah.swift
+++ farah.swift
@@ -5,4 +5,35 @@
 //  Created by Farah mohammed alshammari on 24/03/1445 AH.
 //
 
-import Foundation
+
+import SwiftUI
+
+
+struct farahView: View {
+    var body: some View {
+        
+        VStack(alignment:.center) {
+            
+            Image("img1")
+                .clipShape(Circle())
+                .overlay{
+                                Circle().stroke(.yellow, lineWidth: 4)
+                    
+                            }
+                       
+            Text(" Farah Alshammari")
+            
+                .font(.title)
+                .foregroundColor(.red)
+                .multilineTextAlignment(.leading)
+            Text("28 years old graphic designer , working in F&B sector ")
+                           .font(.subheadline)
+                           .foregroundStyle(.black)
+            
+        }
+    }
+}
+
+#Preview {
+    farahView()
+}
