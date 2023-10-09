46527bcee111c59942097857f55bd346d554c51d
renads22 <roro1x1x@gmail.com>
2023-10-09 11:35:59 +0300

    Renad commit

--- renad.swift
+++ renad.swift
@@ -1,8 +1,33 @@
-//
-//  renad.swift
-//  Profile
-//
-//  Created by Renad alkhathami on 24/03/1445 AH.
-//
+import SwiftUI
 
-import Foundation
+
+struct renadView: View {
+    var body: some View {
+        
+        VStack (alignment: .leading) {
+            
+            Image("R21")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
+                .overlay{
+                                Circle().stroke(.red, lineWidth: 4)
+                            }
+                .padding()
+            Text(" Renad Alkhathami")
+                .font(.title)
+                .foregroundColor(.purple)
+            Text("22 years old girl who is obsessed with games and cats ")
+                           .font(.subheadline)
+                           .foregroundStyle(.black)
+            
+        }
+        
+    }
+}
+
+
+#Preview {
+    renadView()
+}
+
